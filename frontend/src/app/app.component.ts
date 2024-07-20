120c9980dcb0f2788588c8b07d8bd440da523b3a
N. Justus <n-justus@outlook.com>
2024-07-20 11:40:26 +0200

    generate api from openapi

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {DefaultService} from '../../generated-src/person-api';
 
 @Component({
   selector: 'app-root',
@@ -10,4 +11,8 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'person-db';
+
+  constructor(private readonly defaultService: DefaultService) {
+    defaultService.pagedPersons().subscribe(page => console.log(page))
+  }
 }
--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,9 +1,21 @@
-import { ApplicationConfig } from '@angular/core';
+import {ApplicationConfig, importProvidersFrom} from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import {provideHttpClient} from '@angular/common/http';
+import {Configuration, PersonApiModule} from '../../generated-src/person-api';
+import {BrowserModule} from '@angular/platform-browser';
+import {ReactiveFormsModule} from '@angular/forms';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideAnimationsAsync()]
+  providers: [
+    provideHttpClient(),
+    provideRouter(routes),
+    provideAnimationsAsync(),
+    importProvidersFrom(
+      PersonApiModule.forRoot(() => new Configuration({basePath: "/api"})),
+      BrowserModule
+    )
+  ]
 };
