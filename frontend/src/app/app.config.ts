9a54c2d48025880863b2fd66c9ec2fdbbe690949
N. Justus <n-justus@outlook.com>
2024-07-25 19:13:40 +0200

    display paginator

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -7,16 +7,19 @@ import {provideHttpClient} from '@angular/common/http';
 import {Configuration, PersonApiModule} from '../../generated-src/person-api';
 import {BrowserModule} from '@angular/platform-browser';
 import {ReactiveFormsModule} from '@angular/forms';
+import {BrowserAnimationsModule, provideAnimations} from '@angular/platform-browser/animations';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     {provide: LOCALE_ID, useValue: 'de-DE' },
     provideHttpClient(),
     provideRouter(routes),
+    provideAnimations(),
     provideAnimationsAsync(),
     importProvidersFrom(
       PersonApiModule.forRoot(() => new Configuration({basePath: "/api"})),
-      BrowserModule
+      BrowserModule,
+      BrowserAnimationsModule
     )
   ]
 };
--- frontend/src/app/persons/persons.component.ts
+++ frontend/src/app/persons/persons.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import {PersonTableComponent} from '../person-table/person-table.component';
 import {DefaultService, Person} from '../../../generated-src/person-api';
+import {MatPaginator} from '@angular/material/paginator';
 
 @Component({
   selector: 'app-persons',
   standalone: true,
   imports: [
-    PersonTableComponent
+    PersonTableComponent,
+    MatPaginator
   ],
   templateUrl: './persons.component.html',
   styleUrl: './persons.component.scss'
