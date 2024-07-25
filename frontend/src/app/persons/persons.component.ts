441eb9d0f18396715fb43cf59442d717cd8ed2a9
N. Justus <n-justus@outlook.com>
2024-07-25 19:28:27 +0200

    add pagination to table

--- frontend/src/app/persons/persons.component.ts
+++ frontend/src/app/persons/persons.component.ts
@@ -1,7 +1,26 @@
 import { Component } from '@angular/core';
 import {PersonTableComponent} from '../person-table/person-table.component';
 import {DefaultService, Person} from '../../../generated-src/person-api';
-import {MatPaginator} from '@angular/material/paginator';
+import {MatPaginator, PageEvent} from '@angular/material/paginator';
+
+export class PaginatorOffset {
+  constructor(
+  public readonly pageSize: number,
+  public readonly pageIndex: number,
+  ) {
+  }
+
+  static fromPaginationEvent(event: PageEvent): PaginatorOffset {
+    return new PaginatorOffset(
+      event.pageSize,
+      event.pageIndex
+    )
+  }
+
+  static default(): PaginatorOffset {
+    return new PaginatorOffset(10,0)
+  }
+}
 
 @Component({
   selector: 'app-persons',
@@ -16,8 +35,19 @@ import {MatPaginator} from '@angular/material/paginator';
 export class PersonsComponent {
   persons: Person[] = []
 
+  offset: PaginatorOffset = PaginatorOffset.default()
+
   constructor(private readonly defaultService: DefaultService) {
-    defaultService.pagedPersons().subscribe(page => {
+    this.loadPaginatedData()
+  }
+
+  handlePageEvent(event: PageEvent) {
+    this.offset = PaginatorOffset.fromPaginationEvent(event)
+    this.loadPaginatedData()
+  }
+
+  private loadPaginatedData() {
+    this.defaultService.pagedPersons(this.offset.pageSize, this.offset.pageIndex).subscribe(page => {
       this.persons = page.content
       console.log(page)
     })
--- src/main/kotlin/de/detim/njustus/person_db/person/PersonController.kt
+++ src/main/kotlin/de/detim/njustus/person_db/person/PersonController.kt
@@ -1,9 +1,11 @@
 package de.detim.njustus.person_db.person
 
 import org.springframework.data.domain.Page
+import org.springframework.data.domain.PageRequest
 import org.springframework.data.domain.Pageable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 import java.time.LocalDate
 
@@ -14,11 +16,23 @@ data class PersonFilter(
     val city: String?
 )
 
+data class MyPaginator(
+    val pageSize: Int = 10,
+    val pageIndex: Int = 0,
+)
+
 @RestController("/persons")
 class PersonController(
     private val personService: PersonService
 ) {
 
     @PostMapping("/paged")
-    fun pagedPersons(@RequestBody(required = false) filter: PersonFilter?, pageable: Pageable): Page<Person> = personService.findAll(filter, pageable)
+    fun pagedPersons(
+        @RequestBody(required = false) filter: PersonFilter?,
+        @RequestParam(required = false, defaultValue = "10") pageSize: Int,
+        @RequestParam(required = false, defaultValue = "0") pageIndex: Int
+    ): Page<Person> {
+        val pageable = PageRequest.of(pageIndex+1, pageSize)
+        return personService.findAll(filter, pageable)
+    }
 }
